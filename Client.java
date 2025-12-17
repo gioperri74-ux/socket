54a400e08920fd7b8def4b2ab58d11b3144573dc
gioperri74-ux <gioperri74@gmail.com>
2025-12-17 19:00:33 +0000

    prima versione client server

--- Client.java
+++ Client.java
@@ -1,1 +1,11 @@
+public class Client {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
 
+        int a = 5;
+        int b = 3;
+        int somma = a + b;
+
+        System.out.println("La somma è: " + somma);
+    }
+}
